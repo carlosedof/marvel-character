26db3ca361d864aaa3d608459b9749b5d802a956
Cadu Olivera <caduolivera@Cadus-MacBook-Pro.local>
2020-04-02 10:51:03 -0300

    fix: pagination total fix

--- src/pages/CharacterList/index.tsx
+++ src/pages/CharacterList/index.tsx
@@ -12,6 +12,7 @@ import {FILTER_CHARACTERS, GET_CHARACTERS} from "../../services/queries";
 const CharacterList = (props: any) => {
     const [getCharacters, findAll] = useLazyQuery(GET_CHARACTERS);
     const [characters, setCharacters] = useState<Character[]>([]);
+    const [totalCached, setTotalCached] = useState<number>(0);
     const [characterFiltered, setCharacterFiltered] = useState<Character[]>([]);
     const [searchParam, setSearchParam] = useState<string>('');
     const [getCharacterByName, findByName] = useLazyQuery(FILTER_CHARACTERS);
@@ -44,10 +45,11 @@ const CharacterList = (props: any) => {
             const { data } = findAll.data.charactersWithTotal;
             const { total } = findAll.data.charactersWithTotal;
             setTotal(total);
+            setTotalCached(total);
             setCharacters(data);
             setCharacterFiltered(data);
         }
-    }, [findAll.data, setTotal, setCharacters, setCharacterFiltered]);
+    }, [findAll.data, setTotal, setCharacters, setCharacterFiltered, setTotalCached]);
 
     useEffect( () => {
         if (findByName.data && findByName.data.characters && findByName.data.characters.length > 0) {
@@ -55,17 +57,18 @@ const CharacterList = (props: any) => {
             setTotal(findByName.data.characters.length);
         } else {
             setCharacterFiltered(characters);
-            setTotal(characters.length);
+            setTotal(totalCached)
         }
-    }, [findByName.data, characters, setTotal]);
+    }, [findByName.data, characters, setTotal, totalCached]);
 
     useEffect( () => {
-        if (searchParam) {
+        if (searchParam.trim()) {
             getCharacterByName({ variables: { nameStartsWith: searchParam } })
         } else {
             setCharacterFiltered(characters);
+            setTotal(totalCached);
         }
-    }, [searchParam, characters, getCharacterByName, setCharacterFiltered]);
+    }, [searchParam, characters, getCharacterByName, setCharacterFiltered, totalCached]);
 
     return (
         <Container>
